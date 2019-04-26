f1cfe02b10434da496f1738a3c7421e10cef6afb
dariy <gomzykovad@gmail.com>
2019-04-26 19:41:14 +1000

    start

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task("html", function () {
 });
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -63,8 +63,13 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/s-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
+gulp.task("refresh", function(done) {
+  server.reload();
+  done();
+})
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff, woff2}",
