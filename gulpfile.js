aaca85304ea4aea716735bf5dbc983357dcc7d4d
dariy <gomzykovad@gmail.com>
2019-04-28 04:16:21 +1000

    img

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task("images", function() {
     imagemin.jpegtran({progressive: true}),
     imagemin.svgo()
   ]))
-  .pipe(gulp.dest("source/img"));
+  .pipe(gulp.dest("build/img"));
 });
 gulp.task("webp", function() {
   return gulp.src("source/img/**/*.{png,jpg}")
