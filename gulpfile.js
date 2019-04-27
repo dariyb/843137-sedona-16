88e12f8d811fa47cd5a7a6979f78dd51ceda6d98
dariy <gomzykovad@gmail.com>
2019-04-28 03:52:22 +1000

    исправления

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,6 @@ gulp.task("refresh", function(done) {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff, woff2}",
-    "source/img/**",
     "source/js/**",
     "source/*.ico"
   ], {
@@ -89,6 +88,7 @@ gulp.task("build", gulp.series(
   "clean",
   "copy",
   "css",
+  "images",
   "sprite",
   "html"
 ));
