dfd42b9a2c0601f8ab967e24c6d229c963f9470b
dariy <gomzykovad@gmail.com>
2019-04-28 05:40:43 +1000

    fonts

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ gulp.task("refresh", function(done) {
 })
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/**/*.{woff,woff2}",
     "source/js/**",
     "source/*.ico"
   ], {
